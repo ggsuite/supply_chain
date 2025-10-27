cf699a5ff106dcee9511b97de087bbd16ea56766
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2025-10-27 07:08:29 +0100

    Update copy right header

--- test/schedule_task_test.dart
+++ test/schedule_task_test.dart
@@ -1,11 +1,10 @@
 // @license
-// Copyright (c) 2019 - 2024 Dr. Gabriel Gatzsche. All Rights Reserved.
+// Copyright (c) 2019 - 2024 ggsuite. All Rights Reserved.
 //
 // Use of this source code is governed by terms that can be
 // found in the LICENSE file in the root of this package.
 
 import 'package:supply_chain/src/schedule_task.dart';
-
 import 'package:test/test.dart';
 
 void main() {
